a8b7829c17c8bb7440172dd4bbfdd9a409bc4ad1
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2021-02-03 20:17:15 +0200

    Scoped slots

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
 <template>
   <div id="app">
 
-    <card v-for="(card, idx) in cards" :key="idx">
+    <!--card v-for="(card, idx) in cards" :key="idx">
       <template #header>
         <h1 v-if="idx != 0">{{ card.title }}</h1>
       </template>
       <template #default>
         <p v-if="idx != 1" :style="`color: ${card.color}`">{{ card.text }}</p>
       </template>
-    </card>
+    </card-->
+
+    <list>
+      <template #listItem="listItem">
+        <span style="color: blue">Item {{ listItem.counter }} / {{ listItem.counter1 }}</span>
+      </template>
+    </list>
     
   </div>
 </template>
 
 <script>
 import Card from "./components/Card";
+import List from "./components/List";
 
 export default {
   components: {
-    Card
+    Card, List
   },
   data() {
     return {
---
+++ src/components/List.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <ol>
+      <li v-for="(i, j) in 10" :key="i">
+        <slot name="listItem" :counter="i" :counter1="j">
+          Item {{ i }} / {{ j }}
+        </slot>
+      </li>
+    </ol>
+  </div>
+</template>
