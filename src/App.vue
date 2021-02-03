dd452e7db0f6629c994902bccba72a7fc0b26faa
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2021-02-03 19:36:09 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+
+    <card title="Card 1" text="Text for the Card 1" />
+
+    <card title="Card 2" text="Text for the Card 2" />
+
+    <card title="Card 3" text="Text for the Card 3" />
+    
+  </div>
+</template>
+
+<script>
+import Card from "./components/Card";
+
+export default {
+  components: {
+    Card
+  },
+}
+</script>
---
+++ src/components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <header>
+      <h1>{{ title }}</h1>
+    </header>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    text: String,
+  }
+}
+</script>
+
+<style scoped>
+div {
+  display: block;
+  width: 300px;
+  border: 1px solid black;
+  margin-bottom: 15px;
+  padding: 10px;
+}
+</style>
